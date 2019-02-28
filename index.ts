ec2a764103bce35a942a6e492386b6bc46f2b690
Scott Kirkland <srkirkland@gmail.com>
2019-02-28 10:43:16 -0800

    added db connection

--- index.ts
+++ index.ts
@@ -1,8 +1,44 @@
 import express from 'express';
+import knex from 'knex';
 
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// https://knexjs.org/
+const pg = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'lca',
+    password: 'password',
+    database: 'lca'
+  }
+});
+
+interface AnalysisResult {
+  number: number;
+  title: string;
+}
+
+const getRows = async (): Promise<AnalysisResult[]> => {
+  return await pg.table('analysis').where({ title: 'First' });
+};
+
+app.post('/lcarun', async (req, res) => {
+  const params = req.body;
+
+  const rows = await getRows();
+
+  const bigNum = Math.max(...rows.map(r => r.number));
+
+  console.log(rows);
+
+  // analysis
+
+  res.json({ success: true, params, rows, bigNum });
+});
+
+app.get('/', (req, res) => res.send('Hello World!!!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
