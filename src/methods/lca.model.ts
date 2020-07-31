ea537b346598d748bc2e0cd3f3cac1be484b9b6b
Kaiyan Li <kkyli@ucdavis.edu>
2020-07-31 00:17:17 -0700

    update models

--- src/methods/lca.model.ts
+++ src/methods/lca.model.ts
@@ -3,8 +3,7 @@ export interface RunParams {
   diesel: number; // m3
   gasoline: number; // m3
   jetfuel: number; // m3
-  distance: number; // km
-  biomass: number; // tons
+  distance: number; // mile
 }
 
 export interface LCIresults {
@@ -14,8 +13,9 @@ export interface LCIresults {
   CO2e: number;
   CO: number;
   NOx: number;
-  NMVOC: number;
-  Particulates: number;
+  VOCs: number; // Volatile Organic Compounds
+  PM10: number;
+  PM25: number; // PM 2.5
 }
 
 export interface LCIAresults {
@@ -24,24 +24,5 @@ export interface LCIAresults {
   hh_particulate_air: number;
   eutrophication_air: number;
   eutrophication_water: number;
-  ozone_depletion_air: number;
   smog_air: number;
-  ecotox_airu_fresh_water: number;
-  ecotox_airc_fresh_water: number;
-  ecotox_waterc_fresh_water: number;
-  ecotox_seawaterc_fresh_water: number;
-  ecotox_natsoilc_fresh_water: number;
-  ecotox_agrsoilc_fresh_water: number;
-  hh_urban_air_cancer: number;
-  hh_urban_air_noncanc: number;
-  hh_rural_air_cancer: number;
-  hh_rural_air_noncanc: number;
-  hh_fresh_water_cancer: number;
-  hh_fresh_water_noncanc: number;
-  hh_sea_water_cancer: number;
-  hh_sea_water_noncanc: number;
-  hh_natural_soil_cancer: number;
-  hh_natural_soil_noncanc: number;
-  hh_agric_soil_cancer: number;
-  hh_agric_soil_noncanc: number;
 }
