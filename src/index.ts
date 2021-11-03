bacdde9ea11e629864438669c7dd1d6f0543f334
Kaiyan Li <kkyli@ucdavis.edu>
2021-11-03 04:47:16 -0700

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-import { RunParams } from './lca.model';
-import { lcarun } from './lcarun';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { lifeCycleAnalysis } from './lca';
+import { LcaInputs } from './lca.model';
+const swaggerDocument = require('../swagger.json');
+const url = require('url');
 
-export const runLCA = (params: RunParams) => {
-  return lcarun(params);
-};
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/lca', async (req, res) => {
+  const urlParsed = url.parse(req.url, true);
+  const params: LcaInputs = urlParsed.query;
+  const result = await lifeCycleAnalysis(params);
+  res.status(200).json(result);
+});
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
