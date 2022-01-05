9de99db484aab678295616c56a0bcbcf947a0674
Kaiyan Li <38189032+kaiyanl@users.noreply.github.com>
2022-01-05 08:57:29 -0800

    model.d.ts

---
+++ model.d.ts
@@ -0,0 +1,55 @@
+export interface LcaInputs {
+    technology: string;
+    diesel: number;
+    gasoline: number;
+    jetfuel: number;
+    distance: number;
+    construction: number;
+    equipment: number;
+}
+export interface LcaOutputs {
+    lifeCycleEmissions: LifeCycleEmissions;
+    lifeCycleImpacts: LifeCycleImpacts;
+}
+export interface Lci {
+    index: number;
+    name: string;
+    unit: string;
+    gpo: number;
+    chp: number;
+    gp: number;
+    diesel: number;
+    gasoline: number;
+    jetfuel: number;
+    transport: number;
+    construction: number;
+    equipment: number;
+}
+export interface Traci {
+    index: number;
+    name: string;
+    global_warming_air: number;
+    acidification_air: number;
+    hh_particulate_air: number;
+    eutrophication_air: number;
+    smog_air: number;
+}
+export interface LifeCycleEmissions {
+    CO2: number;
+    CH4: number;
+    N2O: number;
+    CO: number;
+    NOx: number;
+    PM10: number;
+    PM25: number;
+    SOx: number;
+    VOC: number;
+    CI: number;
+}
+export interface LifeCycleImpacts {
+    global_warming_air: number;
+    acidification_air: number;
+    hh_particulate_air: number;
+    eutrophication_air: number;
+    smog_air: number;
+}
